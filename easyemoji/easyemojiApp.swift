5b1d381d9da5a65506c70d32341deb4a075f7ed8
Bhaskar Rijal <mail@bhaskarrijal.me>
2026-02-17 01:10:27 +0545

    Initial Commit

---
+++ easyemoji/easyemojiApp.swift
@@ -0,0 +1,17 @@
+//
+//  easyemojiApp.swift
+//  easyemoji
+//
+//  Created by Bhaskar Rijal on 17/02/2026.
+//
+
+import SwiftUI
+
+@main
+struct easyemojiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
